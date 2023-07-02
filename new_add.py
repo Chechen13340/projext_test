7161e6b2ae66992d0c837dc95660e0f291191b0b
Artem Osipov <tema.osipov@mail.ru>
2023-07-02 23:39:12 +0300

    change tree

--- new_add.py
+++
@@ -1,2 +0,0 @@
-def foo():
-    print('Hello new_Branch')
--- utils.py
+++
@@ -1,9 +0,0 @@
-def decide_exercise():
-    a = 5
-    b = 7
-    d = 3
-    e = 12
-    c =a + b + d + e
-    return c
-
-print(decide_exercise())
